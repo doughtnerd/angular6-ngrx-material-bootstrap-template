adcee9e21056d93f7ee82cbc8bde0b12d2b4a68b
Chris Carlson <ccarlson@berkadia.com>
2018-11-14 20:08:29 -0700

    Removed unused routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,6 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 const routes: Routes = [
-  // { path: 'todos', loadChildren: './todos-feature/todo-feature.module#TodoFeatureModule' },
-  // { path: 'documents', loadChildren: './document-feature/document-feature.module#DocumentFeatureModule' }
 ];
 
 @NgModule({
