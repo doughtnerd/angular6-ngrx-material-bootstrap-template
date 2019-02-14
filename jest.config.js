b5e0845ce4b70c49977e685289ebe0f8889cb4e1
Christopher Carlson <ccarlson@utmidccarlsonmb.gmaccm.com>
2019-02-14 09:42:00 -0700

    Able to run with ng test and npm run test

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  preset: "jest-preset-angular",
+  globals: {
+    "ts-jest": {
+      allowSyntheticDefaultImports: true
+    },
+    __TRANSFORM_HTML__: true
+  },
+  testMatch: ["<rootDir>/src/**/?(*.)+(spec|test).ts"],
+  resetMocks: true,
+  setupTestFrameworkScriptFile: "<rootDir>/jest/setup.ts",
+  transformIgnorePatterns: [`<rootDir>/node_modules/`],
+  transform: {
+    "^.+\\.(ts|html)$":
+      "<rootDir>/node_modules/jest-preset-angular/preprocessor.js",
+    "^.+\\.(ts|js)$": "babel-jest"
+  }
+};
